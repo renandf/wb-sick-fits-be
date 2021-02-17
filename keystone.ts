ce279f86845a612b466c704cd9e315e86b4a18dc
Renan Castro <renandf@gmail.com>
2021-02-17 17:40:08 +1100

    feat: Set Keystone foundation

---
+++ keystone.ts
@@ -0,0 +1,34 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import 'dotenv/config';
+import { User } from './schemas/User';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // stay signed in for this amount of seconds
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: Add data seeding here
+  },
+  lists: createSchema({
+    // Schema items go in here
+    User,
+  }),
+  ui: {
+    // TODO: change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO: Add session values here
+});
